0dd8808935614dc04b933995d3050c49699ae9d6
unknown <m.h.talbizadeh@gmail.com>
2020-08-16 01:21:24 -0700

    exersizeone

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-var start1 = prompt('please write a start1: ');
-var start2 = prompt('please write a start2: ');
-var height1 = prompt('please write a height1: ');
-var height2 = prompt('please write a height2: ');
-var start1 = Number(start1);
-var start2 = Number(start2);
-var height1 = Number(height1);
-var height2  = Number(height2);
+var start1 = 2;
+var height1 = 0;
+var start2 = 1;
+var height2 = 500;
+//Sets the value
 var Kangaroo = [start1,height1 , start2 ,height2];
-var a = Kangaroo[0]  ;
-var b = Kangaroo[2] ;
-var i = 1 ;
-var j = 3 ;
-var c= 0 ;
-var k  = 0 ;
-console.log("a :" + a);
-console.log("b : " + b );
-var Distances = [] ;
-one :while(i=1)
-{
-    a =a + Kangaroo[i];
-    b =b + Kangaroo[j];
-    c = a - b ;
-    console.log("a :" + a);
-    console.log("b : " + b );
-    Distances.push(c) ;
-    k++ ;
-    console.log(Distances);
-    if(k>=4)
+//Coverst a array.
+Compariso(Kangaroo); 
+//call methods
+function Compariso( Kangaroo = [start1,height1 , start2 ,height2])
+{var sumKangarooOne = Kangaroo[0]  ;
+    var sumKangarooTwo = Kangaroo[2] ;
+    var indexI = 1 ;
+    var indexJ = 3 ;
+    var Space= 0 ;
+    var counter  = 0 ;
+    var Distances = [] ;
+    //Build a new array for distances.
+    one :while(indexI<10)
     {
-        console.log("check") ;
-        for(var x = 0 ; x<=Distances.length ; x++)
+        sumKangarooOne += Kangaroo[1];
+        sumKangarooTwo += Kangaroo[indexJ];
+        //Addition of heights.
+        Space = sumKangarooOne - sumKangarooTwo ;
+        //Get the distance between two kangaroos.
+        Space = Math.abs(Space);
+        //Converts its value to positive
+        Distances.push(Space) ;
+        //Add  space to the array.
+        counter++ ;
+        indexI++;
+        if(counter>=4)
         {
-            if(Distances[x]==0)
+            //Because the array  length reaches four.
+            for(var counterOne = 0 ; counterOne<=Distances.length ; counterOne++)
             {
-                console.log("Yes");
-                break one ;
+                //If the distance was zero, find it.
+                if(Distances[counterOne]==0)
+                {
+                    console.log("Yes");
+                    break one ;
+                }
             }
-        }
-        for (var x = 0 ; x<Distances.length ; x ++)
-        {
-            if(Distances[x] < Distances[x+1])
+            for (var counterOne = 0 ; counterOne<Distances.length ; counterOne++)
             {
-                console.log("No");
-                break one ;
-            }
-            else if (Distances[x]>= Distances[x+1])
-            {
-                continue one  ;
+                if(Distances[counterOne] <= Distances[counterOne+1])
+                {
+                    console.log("No");
+                    break one ;
+                }
+                else if (Distances[counterOne]> Distances[counterOne+1])
+                {
+                    continue one  ;
+                }
+                //If the distances are greater or equal, it means that they do not reach each other.
+                //If the distances are shorter, it means that they do not reach each other.
             }
         }
     }
